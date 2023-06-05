620f21a26341dcc98ecdc97ad2b771b16b3fd178
Anubhav Chachra <chachraanubhav@gmail.com>
2023-06-05 11:57:51 +0530

    tag search

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -66,11 +66,16 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
       </div>
 
       <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
-      <p
-        className='font-inter text-sm blue_gradient cursor-pointer'
-        onClick={() => handleTagClick && handleTagClick(post.tag)}
-      >
-        #{post.tag}
+      <p>
+        {post.tag.split(/\s+/).map((tag, index) => {
+          const tagWithoutHash = tag.replace(/#/g, "");
+          return (
+            <span key={index} className='font-inter text-sm blue_gradient cursor-pointer' 
+            onClick={() => handleTagClick && handleTagClick(tagWithoutHash)}>
+              #{tagWithoutHash}{" "}
+            </span>
+          )
+        })}
       </p>
 
       {session?.user.id === post.creator._id && pathName === "/profile" && (
